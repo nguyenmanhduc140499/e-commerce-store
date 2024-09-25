34be1a1d1e1ca306ca975f94b64c058c67bdb066
nguyen manh duc <nguyenmanhduc406@gmail.com>
2024-09-25 07:23:54 +0700

    environment variable

--- lib/actions/actions.ts
+++ lib/actions/actions.ts
@@ -1,34 +1,34 @@
 export const getCollections = async () => {
-    const collections = await fetch(`${process.env.NEXT_PUBLIC_API_ADMIN_URL}/collections`, { cache: "no-cache" })
+    const collections = await fetch(`${process.env.NEXT_PUBLIC_API_ADMIN_URL}/api/collections`, { cache: "no-cache" })
     return await collections.json()
 }
 
 export const getCollectionDetails = async (collectionId: string) => {
-    const collection = await fetch(`${process.env.NEXT_PUBLIC_API_ADMIN_URL}/collections/${collectionId}`, { cache: "no-cache" })
+    const collection = await fetch(`${process.env.NEXT_PUBLIC_API_ADMIN_URL}/api/collections/${collectionId}`, { cache: "no-cache" })
     return await collection.json()
 }
 
 export const getProducts = async () => {
-    const products = await fetch(`${process.env.NEXT_PUBLIC_API_ADMIN_URL}/products`, { cache: "no-cache" })
+    const products = await fetch(`${process.env.NEXT_PUBLIC_API_ADMIN_URL}/api/products`, { cache: "no-cache" })
     return await products.json()
 }
 
 export const getProductDetails = async (productId: string) => {
-    const productDetails = await fetch(`${process.env.NEXT_PUBLIC_API_ADMIN_URL}/products/${productId}`, { cache: "no-cache" })
+    const productDetails = await fetch(`${process.env.NEXT_PUBLIC_API_ADMIN_URL}/api/products/${productId}`, { cache: "no-cache" })
     return await productDetails.json()
 }
 
 export const getSearchedProducts = async (query: string) => {
-    const searchedProducts = await fetch(`${process.env.NEXT_PUBLIC_API_ADMIN_URL}/search/${query}`, { cache: "no-cache" })
+    const searchedProducts = await fetch(`${process.env.NEXT_PUBLIC_API_ADMIN_URL}/api/search/${query}`, { cache: "no-cache" })
     return await searchedProducts.json()
 }
 
 export const getOrdersByCustomer = async (customerId: string) => {
-    const ordersByCustomer = await fetch(`${process.env.NEXT_PUBLIC_API_ADMIN_URL}/orders/customers/${customerId}`, { cache: "no-cache" })
+    const ordersByCustomer = await fetch(`${process.env.NEXT_PUBLIC_API_ADMIN_URL}/api/orders/customers/${customerId}`, { cache: "no-cache" })
     return await ordersByCustomer.json()
 }
 
 export const getRelatedProducts = async (productId: string) => {
-    const relatedProducts = await fetch(`${process.env.NEXT_PUBLIC_API_ADMIN_URL}/products/${productId}/related`, { cache: "no-cache" })
+    const relatedProducts = await fetch(`${process.env.NEXT_PUBLIC_API_ADMIN_URL}/api/products/${productId}/related`, { cache: "no-cache" })
     return await relatedProducts.json()
 }
